8150315db5322e47e9e212de3e8176585e795ff6
JuanBeltran2024 <159384302+JuanBeltran2024@users.noreply.github.com>
2024-09-15 21:12:24 -0500

    Update ultrasonido.v

--- CODIGOS/ultrasonido.v
+++ CODIGOS/ultrasonido.v
@@ -2,19 +2,18 @@ module ultrasonido(
     input clk,
     output trigger,
     input echo,
-	 input reset,
+    input reset,
     output state
-	 
-
 );
 
+//--------------------------------Registros-------------------------------------------\\
  reg [25:0] count;
  reg n_trigger;
  reg old_echo;
  reg [32:0] count_out;
  reg [32:0] echo_count;
  reg [32:0] status;
-
+//------------------------------------------------------------------------------------\\
 
  initial begin
     count = 25'b0;
@@ -26,6 +25,7 @@ module ultrasonido(
 
  end
 
+//--------------------------------------------------Logica del ultrasonido-------------------------------------\\
  always @(posedge clk) begin
     if(!reset)begin 
       count_out <= 0;
@@ -35,21 +35,24 @@ module ultrasonido(
       n_trigger <=0;
       end else begin
       count = count + 1;
+     //reduccion AND negado, Generació del tigger, activa el tigger durante cierto tiempo
       n_trigger = ~&(count[24:10]);
-    
+  // Comprobación del tiempo de disparo  
     if (~&(count[24:11])) begin
         if (n_trigger) begin
             if (echo == 1) begin
                 echo_count = echo_count + 1;
             end
+	// Reinicio del contador si se detecta un cambio en el registro echo
             if ((old_echo == 1) && (echo == 0)) begin
                 count_out = echo_count;
                 echo_count = 0;
             end
         end
     end
-    
+    // actualizació del registro estados 
     if (count_out > 33'd15000) begin
+	//desplaza el estado a la izquierda
         status = status << 1;
         status[0] = 1;
     end else begin
@@ -57,10 +60,13 @@ module ultrasonido(
         status[0] = 0;
         
     end
-    
+   // Se guarda un valor de echo para comparar en el siguiente ciclo 
     old_echo = echo;
  end
  end
+//---------------------------------------------------------------------------------------------------------------------\\
+
+// asignacion de registros
  assign trigger = n_trigger;
  assign state = |status;
 
